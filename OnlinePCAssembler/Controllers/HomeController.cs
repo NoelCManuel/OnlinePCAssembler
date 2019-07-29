653ee96d1406fa69c19e82adc392781c2bc5af3c
NoelCManuel <53173840+NoelCManuel@users.noreply.github.com>
2019-07-29 14:07:01 +0530

    sql connection established

--- OnlinePCAssembler/Controllers/HomeController.cs
+++ OnlinePCAssembler/Controllers/HomeController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlinePCAssembler.Models;
+using System.Data.SqlClient;
 
 namespace OnlinePCAssembler.Controllers
 {
     public class HomeController : Controller
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        [HttpGet]
         public ActionResult Homepage()
         {
             return View();
@@ -16,6 +22,35 @@ namespace OnlinePCAssembler.Controllers
         {
             return View();
         }
+        public ActionResult Verify(Account acc)
+        {
+            connectionSting();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from login where username='" + acc.username + "','" + acc.password + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
 
+            }
+            else
+            {
+
+            }
+            con.Close();
+            return View();
+        }
+        void connectionSting()
+        {
+            con.ConnectionString = "server=localhost;user id=root;database=testdb;allowuservariables=True";
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
